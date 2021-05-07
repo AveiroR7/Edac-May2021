c55d391337e69ba8be813c362005903e45df3435
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2021-05-07 17:23:36 +0530

    init first.java

---
+++ Typecasting/first.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class first
+{
+	public static void main(String args[])
+	{
+		byte x;
+		int a =270;
+		double b= 128.128;
+		x = (byte)a;
+		System.out.println("a and x : " + a + " " + x);
+		a = (int)b;
+		System.out.println("b and a : " + b + " " + a );
+		x = (byte)b;
+		System.out.println("b and x : " + b + " " + x );
+	}
+}
